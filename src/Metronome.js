8b5dfbc969f46c894d2eaf119cc4cba548b7f6be
Madhuri Gubbala <gmadhuri@ucsd.edu>
2018-10-14 01:13:41 -0700

    Add metronome slider and start button

--- src/Metronome.js
+++ src/Metronome.js
@@ -3,7 +3,18 @@ import './Metronome.css';
 
 class Metronome extends Component {
     render() {
-        return <div className="metronome">hi</div>;
+        let bpm = 100;
+        let playing = false;
+
+        return (
+            <div className="metronome">
+                <div className="bpm-slider">
+                    <div>{bpm} BPM</div>
+                    <input type="range" min="60" max="240" value={bpm} />
+                </div>
+                <button>{playing ? 'Stop' : 'Start'}</button>
+            </div>
+        );
     }
 }
 
