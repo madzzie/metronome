187183cb33cc9fc76ceb20ea1f625fa671f2ed3f
Madhuri Gubbala <gmadhuri@ucsd.edu>
2018-10-14 01:12:34 -0700

    Update App with Metronome files

---
+++ src/Metronome.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import './Metronome.css';
+
+class Metronome extends Component {
+    render() {
+        return <div className="metronome">hi</div>;
+    }
+}
+
+export default Metronome;
