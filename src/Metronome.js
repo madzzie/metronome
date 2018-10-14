3e11e963b85e7c8fc7d3243555442df99408cf37
Madhuri Gubbala <gmadhuri@ucsd.edu>
2018-10-14 01:19:19 -0700

    Add onChange event for the slider

--- src/Metronome.js
+++ src/Metronome.js
@@ -1,18 +1,41 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './Metronome.css';
 
 class Metronome extends Component {
+    state;
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            playing: false,
+            count: 0,
+            bpm: 100,
+            beatsPerMeasure: 4
+        };
+    }
+
+    handleBpmChange = event => {
+        const bpm = event.target.value;
+        this.setState({bpm});
+    };
+
     render() {
-        let bpm = 100;
-        let playing = false;
+        const {playing, bpm} = this.state;
 
         return (
             <div className="metronome">
                 <div className="bpm-slider">
                     <div>{bpm} BPM</div>
-                    <input type="range" min="60" max="240" value={bpm} />
+                    <input
+                        type="range"
+                        min="60"
+                        max="240"
+                        value={bpm}
+                        onChange={this.handleBpmChange}/>
                 </div>
-                <button>{playing ? 'Stop' : 'Start'}</button>
+                <button>
+                    {playing ? 'Stop' : 'Start'}
+                </button>
             </div>
         );
     }
